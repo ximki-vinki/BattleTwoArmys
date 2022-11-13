81e3b0e5ce0a002f59df42e973ba672237fcb063
Илья Манютин <chem-ixtrems@mail.ru>
2022-11-13 21:04:33 +0300

    Вывод двух армий исходя из разного количеств подразделения

--- BattleTwoArmys/ViewModel/Prints.cs
+++ BattleTwoArmys/ViewModel/Prints.cs
@@ -1,115 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Microsoft.VisualBasic;
-using BattleTwoArmys.Base;
+﻿using BattleTwoArmys.Base;
 using BattleTwoArmys.Model;
 
+namespace BattleTwoArmys.ViewModel;
 
-namespace BattleTwoArmys.ViewModel
+public class Prints
 {
-    public class Prints
-    {
-        private const int CursorPosition = 50;
+    private const int CursorPosition = 50;
 
-        public static void UnitAllInformation(Unit unit)
+    public static void UnitAllInformation(Unit unit)
+    {
+        PrintConsole.Line.AllMinus();
+        UnitParameter.Name(unit);
+        Console.Write("\n");
+        UnitParameter.HitPoint(unit);
+        Console.Write("\n");
+        UnitParameter.Damage(unit);
+        Console.Write("\n");
+        PrintConsole.Line.AllMinus();
+    }
+
+    public static void UnitAllInformation(Unit firstUnit, Unit secondUnit)
+    {
+        PrintConsole.Line.AllMinus();
+        UnitParameter.Name(firstUnit, secondUnit);
+        UnitParameter.HitPoint(firstUnit, secondUnit);
+        UnitParameter.Damage(firstUnit, secondUnit);
+        PrintConsole.Line.AllMinus();
+    }
+
+    public static void UnitAllInformation(Unit unit, bool isRightPosition)
+    {
+        if (isRightPosition)
         {
             PrintConsole.Line.AllMinus();
+            Console.SetCursorPosition(CursorPosition, Console.CursorTop);
             UnitParameter.Name(unit);
             Console.Write("\n");
+            Console.SetCursorPosition(CursorPosition, Console.CursorTop);
             UnitParameter.HitPoint(unit);
             Console.Write("\n");
+            Console.SetCursorPosition(CursorPosition, Console.CursorTop);
             UnitParameter.Damage(unit);
             Console.Write("\n");
             PrintConsole.Line.AllMinus();
         }
-        public static void UnitAllInformation(Unit firstUnit, Unit secondUnit)
+        else
         {
-            PrintConsole.Line.AllMinus();
-            UnitParameter.Name(firstUnit,secondUnit);
-            UnitParameter.HitPoint(firstUnit, secondUnit);
-            UnitParameter.Damage(firstUnit, secondUnit);
-            PrintConsole.Line.AllMinus();
+            UnitAllInformation(unit);
         }
-        
-        public class UnitParameter
+    }
+
+    public static void ChoseLanguage()
+    {
+        Console.WriteLine(StringsMenu.GetString("ChoseLanguage"));
+        Console.WriteLine("1.{0} ", StringsMenu.GetString("Language", "en-EN"));
+        Console.WriteLine("2.{0} ", StringsMenu.GetString("Language", "ru-Ru"));
+    }
+
+    public static void ChoseType(Type type)
+    {
+        var typeString = type.Name;
+        switch (typeString)
         {
-            public static void Name(Unit unit)
-            {
-                Console.Write("{0} : {1}", StringsUnits.GetString("Name"), NameType.Get(unit.GetType()));
-            }
-            public static void Name(Unit firstUnit,Unit secondUnit)
-            {
-                UnitParameter.Name(firstUnit);
-                Console.SetCursorPosition(CursorPosition, Console.CursorTop);
-                UnitParameter.Name(secondUnit);
-                Console.Write("\n");
-            }
-
-            public static void HitPoint(Unit unit)
-            {
-                Console.Write("{0} : {1}", StringsUnits.GetString("HitPoints"), unit.HitPoint);
-            }
-            public static void HitPoint(Unit firstUnit, Unit secondUnit)
-            {
-                UnitParameter.HitPoint(firstUnit);
-                Console.SetCursorPosition(CursorPosition, Console.CursorTop);
-                UnitParameter.HitPoint(secondUnit);
-                Console.Write("\n");
-            }
-
-            public static void Damage(Unit unit)
-            {
-                Console.Write("{0} : {1}", StringsUnits.GetString("Damage"), unit.AllAttacks.First().Value);
-            }
-            public static void Damage(Unit firstUnit, Unit secondUnit)
-            {
-                UnitParameter.Damage(firstUnit);
-                Console.SetCursorPosition(CursorPosition, Console.CursorTop);
-                UnitParameter.Damage(secondUnit);
-                Console.Write("\n");
-            }
+            case "Unit":
+                Console.WriteLine(StringsMenu.GetString("ChoseUnit"));
+                break;
+            default:
+                throw new Exception("No such type for chose");
         }
+    }
 
-        public static void ChoseLanguage()
-        {
-            Console.WriteLine(StringsMenu.GetString("ChoseLanguage"));
-            Console.WriteLine("1.{0} ", StringsMenu.GetString("Language", "en-EN"));
-            Console.WriteLine("2.{0} ", StringsMenu.GetString("Language", "ru-Ru"));
-        }
-
-        public static void ChoseType(Type type)
-        {
-            var typeString = type.Name.ToString();
-            switch (typeString)
+    public static void TwoArmy((Army first, Army second) army)
+    {
+        PrintConsole.Line.AllMinus();
+        OneLineForTwoArmy(army.first.Name, army.second.Name);
+        var maxCountUnit = Math.Max(army.first.UnitList.Count, army.second.UnitList.Count);
+        var minCountUnit = Math.Min(army.first.UnitList.Count, army.second.UnitList.Count);
+        for (var i = 0; i < maxCountUnit; i++)
+            if (i < minCountUnit)
             {
-                case "Unit":
-                    Console.WriteLine(StringsMenu.GetString("ChoseUnit"));
-                    break;
-                default:
-                    throw new Exception("No such type for chose");
+                UnitAllInformation(army.first.UnitList[i], army.second.UnitList[i]);
             }
+            else
+            {
+                var printRightArmy = army.first.UnitList.Count < army.second.UnitList.Count;
+                if (printRightArmy)
+                    UnitAllInformation(army.second.UnitList[i], printRightArmy);
+                else
+                    UnitAllInformation(army.first.UnitList[i]);
+            }
+    }
+
+    private static void OneLineForTwoArmy(string firstArmyString, string secondArmyString)
+    {
+        Console.Write(firstArmyString);
+        Console.SetCursorPosition(CursorPosition, Console.CursorTop);
+        Console.Write(secondArmyString);
+        Console.Write("\n");
+    }
+
+    public class UnitParameter
+    {
+        public static void Name(Unit unit)
+        {
+            Console.Write("{0} : {1}", StringsUnits.GetString("Name"), NameType.Get(unit.GetType()));
         }
 
-        public static void TwoArmy((Unit first, Unit second) army)
+        public static void Name(Unit firstUnit, Unit secondUnit)
         {
-            PrintConsole.Line.AllMinus();
-            OneLineForTwoArmy(StringsMenu.GetString("RedArmy"),StringsMenu.GetString("GreenArmy"));
-            UnitAllInformation(army.first,army.second);
-           
-        }
-
-        private static void OneLineForTwoArmy(string firstArmyString, string secondArmyString)
-        {
-            Console.Write(firstArmyString);
+            Name(firstUnit);
             Console.SetCursorPosition(CursorPosition, Console.CursorTop);
-            Console.Write(secondArmyString);
+            Name(secondUnit);
             Console.Write("\n");
         }
-        //public static void Battle()
+
+        public static void HitPoint(Unit unit)
+        {
+            Console.Write("{0} : {1}", StringsUnits.GetString("HitPoints"), unit.HitPoint);
+        }
+
+        public static void HitPoint(Unit firstUnit, Unit secondUnit)
+        {
+            HitPoint(firstUnit);
+            Console.SetCursorPosition(CursorPosition, Console.CursorTop);
+            HitPoint(secondUnit);
+            Console.Write("\n");
+        }
+
+        public static void Damage(Unit unit)
+        {
+            Console.Write("{0} : {1}", StringsUnits.GetString("Damage"), unit.AllAttacks.First().Value);
+        }
+
+        public static void Damage(Unit firstUnit, Unit secondUnit)
+        {
+            Damage(firstUnit);
+            Console.SetCursorPosition(CursorPosition, Console.CursorTop);
+            Damage(secondUnit);
+            Console.Write("\n");
+        }
     }
+    //public static void Battle()
 }
